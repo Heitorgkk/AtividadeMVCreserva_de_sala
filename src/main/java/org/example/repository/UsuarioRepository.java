3a0f26b29d4552e095e05b611aa27e061a30048d
heitor_klimkowski <heitor_klimkowski@estudante.sesisenai.org.br>
2026-05-15 19:33:45 -0300

    Usuário repository + Usuario não encontrado expeption

--- src/main/java/org/example/repository/UsuarioRepository.java
+++ src/main/java/org/example/repository/UsuarioRepository.java
@@ -1,4 +1,39 @@
 package org.example.repository;
 
+import org.example.entity.Usuario;
+import org.example.erros.UsuarioNaoEncontradoException;
+import java.util.HashMap;
+
 public class UsuarioRepository {
+
+    private HashMap<Integer, Usuario> usuarios;
+
+    public UsuarioRepository(){
+        usuarios = new HashMap<>();
+    }
+
+
+    public Usuario[] getUsuarios(){
+        return usuarios.values().toArray(new Usuario[0]);
+    }
+
+    public Usuario getUserByID(long id) throws UsuarioNaoEncontradoException {
+        for (Usuario u : usuarios.values()) if (u.getId() == id) return u;
+        throw new UsuarioNaoEncontradoException("Usuário não encontrado");
+    }
+
+    public Usuario getUserByName(String nome) throws UsuarioNaoEncontradoException {
+        for (Usuario u : usuarios.values()) if (u.getNome().equals(nome)) return u;
+        throw new UsuarioNaoEncontradoException("Usuário não encontrado");
+    }
+
+    public Usuario getUserByCPF(String cpf) throws UsuarioNaoEncontradoException {
+        for (Usuario u : usuarios.values()) if (u.getCpf().equals(cpf)) return u;
+        throw new UsuarioNaoEncontradoException("Usuário não encontrado");
+    }
+
+    public void insertUser(Usuario user){
+        usuarios.put(usuarios.size() - 1, user);
+    }
+
 }
--- src/main/java/org/example/service/UsuarioService.java
+++ src/main/java/org/example/service/UsuarioService.java
@@ -1,4 +1,15 @@
 package org.example.service;
 
+import org.example.entity.Usuario;
+
 public class UsuarioService {
+
+    static void cadastrarUsuario(){
+        //TODO chamar View para pedir info (nome, cpf)
+
+
+
+    }
+
+
 }
