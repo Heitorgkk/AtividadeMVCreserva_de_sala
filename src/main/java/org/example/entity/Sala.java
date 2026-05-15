4288214283ae90e4d07dc18ef9e89b1dde0c5541
CHRISTIAN FACHINI <christian_fachini@estudante.sesisenai.org.br>
2026-05-15 19:36:13 -0300

    Classe Sala V2

--- src/main/java/org/example/entity/Sala.java
+++ src/main/java/org/example/entity/Sala.java
@@ -1,4 +1,62 @@
 package org.example.entity;
 
 public class Sala {
+
+    private long id;
+    private int numero;
+    private int capacidade;
+    private boolean disponivel;
+
+
+    public Sala(long id, int numero, int capacidade, boolean disponivel){
+        this.id = id;
+        this.numero = numero;
+        this.capacidade = capacidade;
+        this.disponivel = disponivel;
+    }
+
+    public long getId(){
+        return id;
+    }
+
+    public void setId(long id){
+        this.id = id;
+    }
+
+    public int getNumero(){
+        return numero;
+    }
+
+    public void setNumero(int numero){
+        this.numero = numero;
+    }
+    public int getCapacidade(){
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade){
+        this.capacidade = capacidade;
+    }
+
+    public boolean getDisponivel(){
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel){
+        this.disponivel = disponivel;
+    }
+
+
+    public void exibirDados(){
+        System.out.println("Id :"+ id);
+        System.out.println("Numero: "+numero);
+        System.out.println("Capacidade: "+capacidade);
+
+        if(disponivel){
+            System.out.println("Disponivel");
+        }else{
+            System.out.println("Indisponivel");
+        }
+    }
+
 }
