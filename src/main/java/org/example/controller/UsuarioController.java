84c63a2cd1496912b02f41724f6eed0ce62b4101
artQT <artur_thizon@estudante.sesisenai.org.br>
2026-05-15 19:45:20 -0300

    Criação da view e controller do Usuário

--- src/main/java/org/example/controller/UsuarioController.java
+++ src/main/java/org/example/controller/UsuarioController.java
@@ -1,4 +1,13 @@
 package org.example.controller;
 
+import org.example.service.UsuarioService;
+
 public class UsuarioController {
+
+    UsuarioService usuarioService = new UsuarioService();
+
+    public boolean cadastrarUsuario(){
+//        return usuarioService.
+        return true;
+    }
 }
--- src/main/java/org/example/view/UsuarioView.java
+++ src/main/java/org/example/view/UsuarioView.java
@@ -1,4 +1,31 @@
 package org.example.view;
 
+import org.example.controller.UsuarioController;
+import org.example.entity.Usuario;
+
+import java.util.Scanner;
+
 public class UsuarioView {
+
+    Scanner input;
+    UsuarioController usuarioController;
+
+    public UsuarioView(){
+        input = new Scanner(System.in);
+        usuarioController = new UsuarioController();
+    }
+
+    public void cadastro(){
+        System.out.println("Insira seu nome: ");
+        String nome = input.nextLine();
+        System.out.println("Insira seu cpf: ");
+        String cpf = input.nextLine();
+
+        boolean sucesso = usuarioController.cadastrarUsuario();
+        if(sucesso){
+            System.out.println("Usuário Cadastrado com sucesso");
+        }else{
+            System.out.println("Usuário não foi possível ser cadastrado");
+        }
+    }
 }
