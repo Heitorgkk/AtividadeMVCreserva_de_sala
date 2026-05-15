87fb8a960572f3007e3c08f1b09926c833d059be
CHRISTIAN FACHINI <christian_fachini@estudante.sesisenai.org.br>
2026-05-15 19:07:18 -0300

    Classe Usuário

--- src/main/java/org/example/entity/Usuario.java
+++ src/main/java/org/example/entity/Usuario.java
@@ -1,4 +1,44 @@
 package org.example.entity;
 
 public class Usuario {
+
+    private long id;
+    private String nome;
+    private String cpf;
+
+    public Usuario (long id, String nome, String cpf){
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public long getId(){
+        return id;
+    }
+
+    public void setId(long id){
+        this.id = id;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getCpf(){
+        return cpf;
+    }
+
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+
+    public void exibirDados(){
+        System.out.println("Id: "+ id);
+        System.out.println("Nome: "+ nome);
+        System.out.println("CPF: "+ cpf);
+    }
 }
